e31c45c8e75cb16bc2b1ed31b24ef01c42a13686
Charlie Billadeau <charlie.billadeau@gmail.com>
2020-07-20 15:02:36 -0500

    first commit

---
+++ client/build/precache-manifest.f74ea6b03df47db3acec52f72c7784a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d681542ba5d714d8b815641ff70a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc795012f44469c44e5",
+    "url": "/static/css/main.ee45eb7d.chunk.css"
+  },
+  {
+    "revision": "8fe5b1ce939e02f4884c",
+    "url": "/static/js/2.5b11f225.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b11f225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc795012f44469c44e5",
+    "url": "/static/js/main.6e432d56.chunk.js"
+  },
+  {
+    "revision": "4d75d2af8234f3d20308",
+    "url": "/static/js/runtime-main.38cb2ff9.js"
+  }
+]);
